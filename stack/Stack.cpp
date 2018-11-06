e09ceae95488f7990afd766307bfa13a24e12541
NMarcin <marcin.nurzynski97@gmail.com>
2018-11-06 19:45:29 +0100

    Stack interface without implementation
    Stack class created with all standard function (without C++11 emplace
    and swap). Next step is to implement all stack funstions.

--- stack/Stack.cpp
+++ stack/Stack.cpp
@@ -1,1 +1,25 @@
 #include "Stack.hpp"
+
+bool Stack::empty()
+{
+
+}
+int Stack::size()
+{
+
+}
+
+Node* Stack::top()
+{
+
+}
+
+void Stack::push()
+{
+
+}
+
+void Stack::pop(int value)
+{
+
+}
--- stack/Stack.hpp
+++ stack/Stack.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <Node.hpp>
+
 class Stack
 {
+public:
+    bool empty();
+    int size();
+    Node* top();
+    void push();
+    void pop(int value);
 
+    Stack() = default;
+    ~Stack() = default;
+
+private:
+    Node* top_ = nullptr;
 };
